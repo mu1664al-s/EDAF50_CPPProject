684d6bb61fb87f7b84e311d90adee9b8c4a325c7
mu1664al-s <albayati96@gmail.com>
2021-03-23 01:02:09 +0100

    more skeletons

--- mu1664al-s/src/libclientserver/dbdisk.h
+++ mu1664al-s/src/libclientserver/dbdisk.h
@@ -8,6 +8,19 @@ class DBDisk : public DBInterface
 public:
     ~DBDisk();
     DBDisk() = default;
+    bool writeArticle(ID group, Article article) = 0;
+
+    Article readArticle(ID group, ID article) = 0;
+
+    Groups readGroups() = 0;
+
+    void writeGroup(string name) = 0;
+
+    Articles readArticles(ID group) = 0;
+
+    bool deleteArticle(ID group, ID article) = 0;
+
+    bool deleteGroup(ID group) = 0;
 };
 
 #endif
